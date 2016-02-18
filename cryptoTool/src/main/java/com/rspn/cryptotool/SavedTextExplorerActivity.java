5f83b2aa7449f17973fd396813205f3aa9ab7819
Santiago <rsperezn@gmail.com>
2016-02-18 21:35:40 +1100

    Cleaned SavedTextExplorerActivity class

--- cryptoTool/src/main/java/com/rspn/cryptotool/SavedTextExplorerActivity.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/SavedTextExplorerActivity.java
@@ -23,127 +23,123 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class SavedTextExplorerActivity extends ListActivity{
+public class SavedTextExplorerActivity extends ListActivity {
 
-	List<Text> texts;
-	TextSamplesDataSource datasource;
-	Text textToDelete;
-	AdView adView;
+    List<Text> texts;
+    TextSamplesDataSource datasource;
+    Text textToDelete;
+    AdView adView;
 
-	private static final int MENU_DELETE_ID=100;
+    private static final int MENU_DELETE_ID = 100;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_explore_savedtexts);
-		datasource = new TextSamplesDataSource(this);
-		datasource.open();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_savedtexts);
+        datasource = new TextSamplesDataSource(this);
+        datasource.open();
 
-		//Ads
-		adView = (AdView) this.findViewById(R.id.adView_InExploreSavedTextsActivity);
-		AdRequest adRequest = new AdRequest.Builder().build();
-		adView.loadAd(adRequest);
+        //Ads
+        adView = (AdView) this.findViewById(R.id.adView_InExploreSavedTextsActivity);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
-		texts=datasource.findAll();
-		refreshDisplay();
-		registerForContextMenu(getListView());
-	}
+        texts = datasource.findAll();
+        refreshDisplay();
+        registerForContextMenu(getListView());
+    }
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo)menuInfo;
-		int idSelected = (int)info.id;
-		textToDelete= texts.get(idSelected);
-		if(textToDelete.getDeletable()==1){
-			menu.add(0,MENU_DELETE_ID,1,"Delete");
-		}
-	}
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+        int idSelected = (int) info.id;
+        textToDelete = texts.get(idSelected);
+        if (textToDelete.getDeletable() == 1) {
+            menu.add(0, MENU_DELETE_ID, 1, "Delete");
+        }
+    }
 
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		if(item.getItemId()==MENU_DELETE_ID){
-			boolean result = datasource.delete(textToDelete.getId());
-			if(result){
-				Toast.makeText(this, "Deleted Succesfully", Toast.LENGTH_SHORT).show();
-			}
-			else{
-				Toast.makeText(this, "Failed Deleting", Toast.LENGTH_SHORT).show();
-			}
-			refreshDisplay();	
-		}
-
-
-		return super.onContextItemSelected(item);
-	}
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == MENU_DELETE_ID) {
+            boolean result = datasource.delete(textToDelete.getId());
+            if (result) {
+                Toast.makeText(this, "Deleted Succesfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Failed Deleting", Toast.LENGTH_SHORT).show();
+            }
+            refreshDisplay();
+        }
 
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Intent intent = new Intent(this,SavedTextViewerActivity.class);
-		intent.putExtra("text", texts.get(position).getContent());
-		startActivity(intent);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.explore_savedtexts, menu);
-		//TODO add action bar http://stackoverflow.com/questions/13875090/actionbar-and-listactivity-in-one-activity
-		//getActionBar().setDisplayHomeAsUpEnabled(true);
-		return super.onCreateOptionsMenu(menu);
-	}
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if(id==android.R.id.home){
-			finish();
-			return true;
-		}
-		if(id==R.id.action_ExploreAll){
-			texts=datasource.findAll();
-			refreshDisplay();
-			return true;
-		}
-		if(id==R.id.action_ExplorePlainText){
-			texts=datasource.findFiltered("type='"+CTUtils.PT+"'", "title DESC");
-			refreshDisplay();
-			return true;
-		}
-
-		if(id==R.id.action_ExploreDecrypted){
-			texts=datasource.findFiltered("type='"+CTUtils.DT+"'", "title DESC");
-			refreshDisplay();
-			return true;
-		}
-		if(id==R.id.action_ExploreEncrypted){
-			texts=datasource.findFiltered("type='"+CTUtils.ET+"'", "title DESC");
-			refreshDisplay();
-			return true;
-		}
-
-		if(id==R.id.action_ExploreBrokenEncrypted){
-			texts=datasource.findFiltered("type='"+CTUtils.BET+"'", "title DESC");
-			refreshDisplay();
-			return true;
-		}
-
-		if(id==R.id.action_ExploreSamples){
-			texts=datasource.findSamples();
-			refreshDisplay();
-			return true;
-		}
+        return super.onContextItemSelected(item);
+    }
 
 
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(this, SavedTextViewerActivity.class);
+        intent.putExtra("text", texts.get(position).getContent());
+        startActivity(intent);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.explore_savedtexts, menu);
+        //TODO add action bar http://stackoverflow.com/questions/13875090/actionbar-and-listactivity-in-one-activity
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        if (id == R.id.action_ExploreAll) {
+            texts = datasource.findAll();
+            refreshDisplay();
+            return true;
+        }
+        if (id == R.id.action_ExplorePlainText) {
+            texts = datasource.findFiltered("type='" + CTUtils.PT + "'", "title DESC");
+            refreshDisplay();
+            return true;
+        }
 
-	private void refreshDisplay() {
-		ArrayAdapter<Text> adapter = new SavedTextArrayAdapter(this, texts);
-		setListAdapter(adapter);	
-	}
+        if (id == R.id.action_ExploreDecrypted) {
+            texts = datasource.findFiltered("type='" + CTUtils.DT + "'", "title DESC");
+            refreshDisplay();
+            return true;
+        }
+        if (id == R.id.action_ExploreEncrypted) {
+            texts = datasource.findFiltered("type='" + CTUtils.ET + "'", "title DESC");
+            refreshDisplay();
+            return true;
+        }
 
+        if (id == R.id.action_ExploreBrokenEncrypted) {
+            texts = datasource.findFiltered("type='" + CTUtils.BET + "'", "title DESC");
+            refreshDisplay();
+            return true;
+        }
+
+        if (id == R.id.action_ExploreSamples) {
+            texts = datasource.findSamples();
+            refreshDisplay();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refreshDisplay() {
+        ArrayAdapter<Text> adapter = new SavedTextArrayAdapter(this, texts);
+        setListAdapter(adapter);
+    }
 
 }
