ddd91a7ecf8621adf80a21292f2c954f56bc101d
Santiago <rsperezn@gmail.com>
2016-02-18 21:29:44 +1100

    Cleaned OpenDialogFragment class

--- cryptoTool/src/main/java/com/rspn/cryptotool/UIHelper/OpenDialogFragment.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/UIHelper/OpenDialogFragment.java
@@ -30,60 +30,60 @@ import android.widget.ListView;
 
 public class OpenDialogFragment extends DialogFragment implements OnItemClickListener {
 
-	String textSampleTypeFilter;
+    String textSampleTypeFilter;
 
     ListView mylist;
     TextSamplesDataSource datasource;
     List<Text> textSamples;
-    HashMap<String, String> titles = new HashMap<String, String>();
-    EditText previewEditText=null;
-    Boolean isComparingHahes=false;
-    
-    public OpenDialogFragment(){
-    	
+    HashMap<String, String> titles = new HashMap<>();
+    EditText previewEditText = null;
+    Boolean isComparingHahes = false;
+
+    public OpenDialogFragment() {
+
     }
-    public OpenDialogFragment(String textSampleTypeFilter){
-    	this.textSampleTypeFilter=textSampleTypeFilter;
+
+    public OpenDialogFragment(String textSampleTypeFilter) {
+        this.textSampleTypeFilter = textSampleTypeFilter;
     }
-    
-    public OpenDialogFragment(EditText previewEditText){
-    	isComparingHahes =true;
-    	this.textSampleTypeFilter="All";
-    	this.previewEditText= previewEditText;       	
+
+    public OpenDialogFragment(EditText previewEditText) {
+        isComparingHahes = true;
+        this.textSampleTypeFilter = "All";
+        this.previewEditText = previewEditText;
     }
-    
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-    	titles.put("EncryptedText", "Encrypted Texts");
-    	titles.put("PlainText", "Plain Texts");
-    	titles.put("All", "All Saved Texts");
-    	
-    	    	
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        titles.put("EncryptedText", "Encrypted Texts");
+        titles.put("PlainText", "Plain Texts");
+        titles.put("All", "All Saved Texts");
+
+
         View view = inflater.inflate(R.layout.activity_open_savedtextdialogfragment, null, false);
         mylist = (ListView) view.findViewById(R.id.list);
 
         registerForContextMenu(view);
         getDialog().setTitle(titles.get(textSampleTypeFilter) + " samples");//title
-        
+
         //database
-        datasource= new TextSamplesDataSource(getActivity());
+        datasource = new TextSamplesDataSource(getActivity());
         datasource.open();
         return view;
     }
 
-    
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
 
         super.onActivityCreated(savedInstanceState);
-        
-        if(textSampleTypeFilter.equals(("All"))){
-        	textSamples = datasource.findAll();
+
+        if (textSampleTypeFilter.equals(("All"))) {
+            textSamples = datasource.findAll();
+        } else {
+            textSamples = datasource.findFiltered("type='" + textSampleTypeFilter + "'", "title ASC");
         }
-        else{ 
-        	textSamples= datasource.findFiltered("type='"+textSampleTypeFilter+"'", "title ASC");
-        }
-    
+
         SavedTextArrayAdapter adapter = new SavedTextArrayAdapter(getActivity(), textSamples);
         mylist.setAdapter(adapter);
         mylist.setOnItemClickListener(this);
@@ -91,38 +91,32 @@ public class OpenDialogFragment extends DialogFragment implements OnItemClickLis
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
-            long id) {
-    	
-    	String selection =textSamples.get(position).getContent().toString();
-    	if(textSampleTypeFilter.equals("PlainText")){//calling activity EncryptActivity
-    		EncryptActivity callingActivity= (EncryptActivity)getActivity();
-    		callingActivity.onUserSelectedTextSample(selection);	
-    	}
-    	
-    	else if( textSampleTypeFilter.equals("EncryptedText")){//calling activity DecryptActivity
-    		if(getActivity().getClass().getSimpleName().equals("BreakEncryptionActivity")){
-    			BreakEncryptionActivity callingActivity= (BreakEncryptionActivity)getActivity();
-        		callingActivity.onUserSelectedTextSample(selection);
-    			
-    		}
-    		else{
-	    		DecryptActivity callingActivity= (DecryptActivity)getActivity();
-	    		callingActivity.onUserSelectedTextSample(selection);
-    		}
-    	}
-    	
-    	else{// All saved Texts called by HashActivity
-    		//TODO check if its comparing hashes of texts or files
-    		if(isComparingHahes){
-    			previewEditText.setText(selection);
-    		}
-    		else{
-    			CalculateHashesActivity callingActivity = (CalculateHashesActivity)getActivity();
-        		callingActivity.setInputForHashCalculation(selection);
-    		} 
-    	}    	
+                            long id) {
+
+        String selection = textSamples.get(position).getContent().toString();
+        if (textSampleTypeFilter.equals("PlainText")) {//calling activity EncryptActivity
+            EncryptActivity callingActivity = (EncryptActivity) getActivity();
+            callingActivity.onUserSelectedTextSample(selection);
+        } else if (textSampleTypeFilter.equals("EncryptedText")) {//calling activity DecryptActivity
+            if (getActivity().getClass().getSimpleName().equals("BreakEncryptionActivity")) {
+                BreakEncryptionActivity callingActivity = (BreakEncryptionActivity) getActivity();
+                callingActivity.onUserSelectedTextSample(selection);
+
+            } else {
+                DecryptActivity callingActivity = (DecryptActivity) getActivity();
+                callingActivity.onUserSelectedTextSample(selection);
+            }
+        } else {// All saved Texts called by HashActivity
+            //TODO check if its comparing hashes of texts or files
+            if (isComparingHahes) {
+                previewEditText.setText(selection);
+            } else {
+                CalculateHashesActivity callingActivity = (CalculateHashesActivity) getActivity();
+                callingActivity.setInputForHashCalculation(selection);
+            }
+        }
         datasource.close();
-    	dismiss();
+        dismiss();
     }
 
 }
