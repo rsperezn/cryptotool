e98ca31ae40614f4cab02699fa5b55a6e0dd94e6
Santiago <rsperezn@gmail.com>
2016-02-18 21:01:39 +1100

    Cleaned CaesarBreaker

--- cryptoTool/src/main/java/com/rspn/cryptotool/breakencryption/CaesarBreaker.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/breakencryption/CaesarBreaker.java
@@ -1,18 +1,15 @@
 package com.rspn.cryptotool.breakencryption;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
-
 import com.rspn.cryptotool.decrypt.CaesarsDecryption;
 import com.rspn.cryptotool.utils.ChiSquared;
+
+import java.util.Collections;
+import java.util.HashMap;
 
 public class CaesarBreaker {
 	
 	public static HashMap<String,Object>  runBreak(String encryptedText, boolean whitespaces){
-		HashMap<Double,Integer> results= new HashMap<Double,Integer>();
+		HashMap<Double,Integer> results= new HashMap<>();
 		String tempDecrypt;
 		double tempFitness;
 		for (int i = 0; i < 26; i++) {
@@ -23,7 +20,7 @@ public class CaesarBreaker {
 		//find best fit and decrypt with such value
 		double minFitness=Collections.min(results.keySet());
 		int shiftVal= results.get(minFitness);
-		HashMap<String,Object>resultsInfo= new HashMap<String,Object>(); //shiftval and actual decrypted text
+		HashMap<String,Object>resultsInfo= new HashMap<>(); //shiftval and actual decrypted text
 		resultsInfo.put("shift",shiftVal);
 		resultsInfo.put("decryptedText",CaesarsDecryption.runDecryption(encryptedText, shiftVal, whitespaces));
 		return resultsInfo;
