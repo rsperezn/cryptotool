29d801cf97d1d4e0f5ecdd63e6a7b621b6222a73
Santiago <rsperezn@gmail.com>
2016-02-18 21:31:08 +1100

    Cleaned SavedTextArrayAdapter  class

--- cryptoTool/src/main/java/com/rspn/cryptotool/UIHelper/SavedTextArrayAdapter.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/UIHelper/SavedTextArrayAdapter.java
@@ -1,12 +1,5 @@
 package com.rspn.cryptotool.UIHelper;
 
-import java.util.List;
-
-import com.rspn.cryptotool.R;
-import com.rspn.cryptotool.R.id;
-import com.rspn.cryptotool.R.layout;
-import com.rspn.cryptotool.model.Text;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,34 +7,37 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.rspn.cryptotool.R;
+import com.rspn.cryptotool.model.Text;
+
+import java.util.List;
+
 public class SavedTextArrayAdapter extends ArrayAdapter<Text> {
-	
-	Context context;
-	List<Text> texts;
-	
-	public SavedTextArrayAdapter(Context context, List<Text> texts){
-		super(context, R.layout.single_row_savedtext,texts);
-		this.context=context;
-		this.texts=texts;
-	}
-	
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View view= inflater.inflate(R.layout.single_row_savedtext, null);
-		
-		Text text = texts.get(position);
-		
-		TextView textView = (TextView) view.findViewById(R.id.TitleTextView);
-		textView.setText(text.getTitle());
-		
-		textView= (TextView) view.findViewById(R.id.ContentTextView);
-		String preview = text.getContent().length() >40? text.getContent().substring(0, 35) +" ..." :text.getContent(); 
-		textView.setText(preview);
-		
-		
-		return view;
-	}
-	
-	
+
+    Context context;
+    List<Text> texts;
+
+    public SavedTextArrayAdapter(Context context, List<Text> texts) {
+        super(context, R.layout.single_row_savedtext, texts);
+        this.context = context;
+        this.texts = texts;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.single_row_savedtext, null);
+
+        Text text = texts.get(position);
+
+        TextView textView = (TextView) view.findViewById(R.id.TitleTextView);
+        textView.setText(text.getTitle());
+
+        textView = (TextView) view.findViewById(R.id.ContentTextView);
+        String preview = text.getContent().length() > 40 ? text.getContent().substring(0, 35) + " ..." : text.getContent();
+        textView.setText(preview);
+
+        return view;
+    }
+
 }
