4e53caed22aac582f2fa0ab3082de385c357f9e5
Santiago <rsperezn@gmail.com>
2016-02-18 21:31:35 +1100

    Cleaned ChiSquared  class

--- cryptoTool/src/main/java/com/rspn/cryptotool/utils/ChiSquared.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/utils/ChiSquared.java
@@ -1,28 +1,28 @@
 package com.rspn.cryptotool.utils;
 
 public class ChiSquared {
-	
-	public static char[] alphabet= CTUtils.getAlphabet();
-	public static double calculate(String text){
-		double sum=0;
-		//format text by removing all spaces special characters and making it upper case
-		text= CTUtils.sanitize(text);
-		int[] charCount= new int[26];//occurrences of each of the characters
-		double[] expected = new double[26];
-		//count
-		for(int i=0 ; i<text.length();i++){
-			charCount[CTUtils.charGetInt(text.charAt(i))]+=1;
-		}
-		//expected occurrence of each character
-		for(int i=0; i<charCount.length;i++ ){
-			expected[i]=text.length()*CTUtils.charGetProb(alphabet[i]);
-		}
-		//calculate
-		for(int i=0;i<26;i++){
-			sum+=Math.pow((charCount[i]-expected[i]),2)/expected[i];
-		}
-		return sum;		
-	}
-	
+
+    public static char[] alphabet = CTUtils.getAlphabet();
+
+    public static double calculate(String text) {
+        double sum = 0;
+        //format text by removing all spaces special characters and making it upper case
+        text = CTUtils.sanitize(text);
+        int[] charCount = new int[26];//occurrences of each of the characters
+        double[] expected = new double[26];
+        //count
+        for (int i = 0; i < text.length(); i++) {
+            charCount[CTUtils.charGetInt(text.charAt(i))] += 1;
+        }
+        //expected occurrence of each character
+        for (int i = 0; i < charCount.length; i++) {
+            expected[i] = text.length() * CTUtils.charGetProb(alphabet[i]);
+        }
+        //calculate
+        for (int i = 0; i < 26; i++) {
+            sum += Math.pow((charCount[i] - expected[i]), 2) / expected[i];
+        }
+        return sum;
+    }
 
 }
