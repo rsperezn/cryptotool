4d496a7e13a48b5b2065edeb141f19c7d5b70158
Santiago <rsperezn@gmail.com>
2016-02-18 21:11:45 +1100

    Cleaned TextSamplesDBOpenHelper class

--- cryptoTool/src/main/java/com/rspn/cryptotool/db/TextSamplesDBOpenHelper.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/db/TextSamplesDBOpenHelper.java
@@ -8,52 +8,52 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class TextSamplesDBOpenHelper extends SQLiteOpenHelper {
-	
-	private static final String DATABASE_NAME = "textsamples.db";
-	private static final int DATABASE_VERSION = 4;
 
-	public static final String TABLE_TEXTSAMPLES = "textsamples";
-	public static final String COLUMN_ID = "ID";
-	public static final String COLUMN_TITLE = "title";
-	public static final String COLUMN_TYPE = "type";
-	public static final String COLUMN_CONTENT = "content";
-	public static final String COLUMN_DELETABLE = "deletable";
+    private static final String DATABASE_NAME = "textsamples.db";
+    private static final int DATABASE_VERSION = 4;
 
-	private static final String TABLE_CREATE = 
-			"CREATE TABLE " + TABLE_TEXTSAMPLES + " (" +
-					COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-					COLUMN_TITLE + " TEXT, " +
-					COLUMN_TYPE + " NUMERIC, " +
-					COLUMN_CONTENT + " TEXT, " +
-					COLUMN_DELETABLE +" NUMERIC "+
-					")";
+    public static final String TABLE_TEXTSAMPLES = "textsamples";
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_TYPE = "type";
+    public static final String COLUMN_CONTENT = "content";
+    public static final String COLUMN_DELETABLE = "deletable";
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_TEXTSAMPLES + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_TITLE + " TEXT, " +
+                    COLUMN_TYPE + " NUMERIC, " +
+                    COLUMN_CONTENT + " TEXT, " +
+                    COLUMN_DELETABLE + " NUMERIC " +
+                    ")";
 
 //	public static final String TABLE_MYTEXTSAMPLES = "mytextsamples";
 //	private static final String TABLE_MYTEXTSAMPLES_CREATE = 
 //			"CREATE TABLE " + TABLE_MYTEXTSAMPLES + " (" +
 //			COLUMN_ID + " INTEGER PRIMARY KEY)";
-	
-	public TextSamplesDBOpenHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
 
-	@Override
-	//Automatically called by SDK
-	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(TABLE_CREATE);
-		//db.execSQL(TABLE_MYTEXTSAMPLES_CREATE);
+    public TextSamplesDBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-		Log.i(CTUtils.TAG, "Table has been created");
-	}
+    @Override
+    //Automatically called by SDK
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+        //db.execSQL(TABLE_MYTEXTSAMPLES_CREATE);
 
-	@Override
-	//Automatically called by SDK
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_TEXTSAMPLES);
-		//db.execSQL("DROP TABLE IF EXISTS " + TABLE_MYTEXTSAMPLES);
-		onCreate(db);
+        Log.i(CTUtils.TAG, "Table has been created");
+    }
 
-		Log.i(CTUtils.TAG, "Database has been upgraded from " + 
-				oldVersion + " to " + newVersion);
-	}
+    @Override
+    //Automatically called by SDK
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TEXTSAMPLES);
+        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_MYTEXTSAMPLES);
+        onCreate(db);
+
+        Log.i(CTUtils.TAG, "Database has been upgraded from " +
+                oldVersion + " to " + newVersion);
+    }
 }
