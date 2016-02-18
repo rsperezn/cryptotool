490e2ef611daaaa21eac5c1c1e32437627311292
Santiago <rsperezn@gmail.com>
2016-02-18 21:27:22 +1100

    Cleaned DrawerAdapter class

--- cryptoTool/src/main/java/com/rspn/cryptotool/UIHelper/DrawerAdapter.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/UIHelper/DrawerAdapter.java
@@ -1,7 +1,9 @@
 package com.rspn.cryptotool.UIHelper;
 
 import java.util.List;
+
 import com.rspn.cryptotool.model.NavigationItem;
+
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -10,54 +12,54 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.rspn.cryptotool.R;
- 
+
 public class DrawerAdapter extends ArrayAdapter<NavigationItem> {
- 
-      Context context;
-      List<NavigationItem> drawerItemList;
-      int layoutResID;
- 
-      public DrawerAdapter(Context context,List<NavigationItem> listItems) {
-            super(context, R.layout.sigle_row_navigation, listItems);
-            this.context = context;
-            this.drawerItemList = listItems;
-            this.layoutResID = R.layout.sigle_row_navigation;
- 
-      }
- 
-      @Override
-      public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
- 
-            DrawerItemHolder drawerHolder;
-            View view = convertView;
- 
-            if (view == null) {
-                  LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-                  drawerHolder = new DrawerItemHolder();
- 
-                  view = inflater.inflate(layoutResID, parent, false);
-                  drawerHolder.ItemName = (TextView) view.findViewById(R.id.title_icon_navigation);
-                  drawerHolder.icon = (ImageView) view.findViewById(R.id.icon_navigation);
- 
-                  view.setTag(drawerHolder);
- 
-            } else {
-                  drawerHolder = (DrawerItemHolder) view.getTag();
- 
-            }
- 
-            NavigationItem dItem = (NavigationItem) this.drawerItemList.get(position);
- 
-            drawerHolder.icon.setImageDrawable(view.getResources().getDrawable(dItem.getImgResID()));
-            drawerHolder.ItemName.setText(dItem.getItemName());
- 
-            return view;
-      }
- 
-      private static class DrawerItemHolder {
-            TextView ItemName;
-            ImageView icon;
-      }
+
+    Context context;
+    List<NavigationItem> drawerItemList;
+    int layoutResID;
+
+    public DrawerAdapter(Context context, List<NavigationItem> listItems) {
+        super(context, R.layout.sigle_row_navigation, listItems);
+        this.context = context;
+        this.drawerItemList = listItems;
+        this.layoutResID = R.layout.sigle_row_navigation;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+
+        DrawerItemHolder drawerHolder;
+        View view = convertView;
+
+        if (view == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            drawerHolder = new DrawerItemHolder();
+
+            view = inflater.inflate(layoutResID, parent, false);
+            drawerHolder.ItemName = (TextView) view.findViewById(R.id.title_icon_navigation);
+            drawerHolder.icon = (ImageView) view.findViewById(R.id.icon_navigation);
+
+            view.setTag(drawerHolder);
+
+        } else {
+            drawerHolder = (DrawerItemHolder) view.getTag();
+
+        }
+
+        NavigationItem dItem = (NavigationItem) this.drawerItemList.get(position);
+
+        drawerHolder.icon.setImageDrawable(view.getResources().getDrawable(dItem.getImgResID()));
+        drawerHolder.ItemName.setText(dItem.getItemName());
+
+        return view;
+    }
+
+    private static class DrawerItemHolder {
+        TextView ItemName;
+        ImageView icon;
+    }
 }
