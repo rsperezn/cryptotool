4996407b4be2be8d3fa602483c2fb4d8d5ae5c45
Santiago <rsperezn@gmail.com>
2016-02-18 21:35:00 +1100

    Cleaned PreferencesActivity class

--- cryptoTool/src/main/java/com/rspn/cryptotool/PreferencesActivity.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/PreferencesActivity.java
@@ -4,25 +4,25 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.view.MenuItem;
 
-public class PreferencesActivity extends PreferenceActivity{
-	
-	@SuppressWarnings("deprecation")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.prefs);
-		//TODO : Add actionbar
-		//getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if(id==android.R.id.home){
-			finish();
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
+public class PreferencesActivity extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.prefs);
+        //TODO : Add actionbar
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
