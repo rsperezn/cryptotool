fb8cfb9ce62e26a399956348beb270f278aa9e54
Santiago <rsperezn@gmail.com>
2016-02-18 21:22:51 +1100

    Cleaned PasswordGenerator class

--- cryptoTool/src/main/java/com/rspn/cryptotool/passwordgenerator/PasswordGenerator.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/passwordgenerator/PasswordGenerator.java
@@ -1,69 +1,70 @@
 package com.rspn.cryptotool.passwordgenerator;
 
+import com.rspn.cryptotool.passwordgenerator.Characters.Types;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import com.rspn.cryptotool.passwordgenerator.Characters.Types;
 
 public class PasswordGenerator {
 
-	public static String generatePassword(int passwordLength, boolean excludeSimilarLookingCharacters, Characters.Types... requiredTypes) throws Exception {
+    public static String generatePassword(int passwordLength, boolean excludeSimilarLookingCharacters, Characters.Types... requiredTypes) throws Exception {
 
 
-		boolean requiresAtLeastOneEachType = requiredTypes.length == Types.values().length;
-		if (!requiresAtLeastOneEachType) {		
-			String strongPassword = "";			
-			for (int i = 0; i < passwordLength; i++) {
-				char randomCharacter =CharacterGenerator.getRandomCharacter(requiredTypes);
-				if(excludeSimilarLookingCharacters &&  CharacterGenerator.isSimilarLookingCharacter(randomCharacter)){
-					randomCharacter = CharacterGenerator.getNonSimilarLookingCharacter();
-				}
-				strongPassword += randomCharacter;
-			}
-			return strongPassword;
-		} else {
-			char[] strongPasswordArr = createPasswordWithRequiredTypes(passwordLength, excludeSimilarLookingCharacters);
+        boolean requiresAtLeastOneEachType = requiredTypes.length == Types.values().length;
+        if (!requiresAtLeastOneEachType) {
+            String strongPassword = "";
+            for (int i = 0; i < passwordLength; i++) {
+                char randomCharacter = CharacterGenerator.getRandomCharacter(requiredTypes);
+                if (excludeSimilarLookingCharacters && CharacterGenerator.isSimilarLookingCharacter(randomCharacter)) {
+                    randomCharacter = CharacterGenerator.getNonSimilarLookingCharacter();
+                }
+                strongPassword += randomCharacter;
+            }
+            return strongPassword;
+        } else {
+            char[] strongPasswordArr = createPasswordWithRequiredTypes(passwordLength, excludeSimilarLookingCharacters);
 
-			// fill the rest of the array with random Characters
-			for (int i = 0; i < passwordLength; i++) {
-				if (strongPasswordArr[i] == '\0') {
-					strongPasswordArr[i] = CharacterGenerator.getRandomCharacter();
-				}
-			}
-			return new String(strongPasswordArr);
-		}
-	}
-	
-	private static char[] createPasswordWithRequiredTypes(int passwordLength, boolean excludeSimilarLookingCharacters) throws Exception {
-		Random random = new Random();
-		List<Types> requiredTypes;
-		char[] strongPasswordArr = new char[passwordLength];
-		requiredTypes =  Arrays.asList(Characters.Types.values());
+            // fill the rest of the array with random Characters
+            for (int i = 0; i < passwordLength; i++) {
+                if (strongPasswordArr[i] == '\0') {
+                    strongPasswordArr[i] = CharacterGenerator.getRandomCharacter();
+                }
+            }
+            return new String(strongPasswordArr);
+        }
+    }
 
-		int characterTypesLength = Characters.getTypesLength();
-		List<Integer> availablePositions = createAvailablePositions(passwordLength);
-		// get a random character of each type and put it at a random position
-		for (int i = 0; i < characterTypesLength; i++) {
-			int positionForRandomCharacter = availablePositions.get(random.nextInt(availablePositions.size()));
-			Types requiredType = requiredTypes.get(i);
-			char randomCharacter = CharacterGenerator.getRandomCharacter(requiredType);
-			if(CharacterGenerator.isSimilarLookingCharacter(randomCharacter) && excludeSimilarLookingCharacters){
-				randomCharacter = CharacterGenerator.getNonSimilarLookingCharacter(requiredType);
-			}
-			
-			strongPasswordArr[positionForRandomCharacter] = randomCharacter;
-			availablePositions.remove((Integer)positionForRandomCharacter);
-		}
-		return strongPasswordArr;
-	}
+    private static char[] createPasswordWithRequiredTypes(int passwordLength, boolean excludeSimilarLookingCharacters) throws Exception {
+        Random random = new Random();
+        List<Types> requiredTypes;
+        char[] strongPasswordArr = new char[passwordLength];
+        requiredTypes = Arrays.asList(Characters.Types.values());
 
-	private static List<Integer> createAvailablePositions(int passwordLength) {
-		List<Integer> availablePositions = new ArrayList<>();
-		for (int i = 0; i < passwordLength; i++) {
-			availablePositions.add(i);
-		}
-		return availablePositions;
-	}
+        int characterTypesLength = Characters.getTypesLength();
+        List<Integer> availablePositions = createAvailablePositions(passwordLength);
+        // get a random character of each type and put it at a random position
+        for (int i = 0; i < characterTypesLength; i++) {
+            int positionForRandomCharacter = availablePositions.get(random.nextInt(availablePositions.size()));
+            Types requiredType = requiredTypes.get(i);
+            char randomCharacter = CharacterGenerator.getRandomCharacter(requiredType);
+            if (CharacterGenerator.isSimilarLookingCharacter(randomCharacter) && excludeSimilarLookingCharacters) {
+                randomCharacter = CharacterGenerator.getNonSimilarLookingCharacter(requiredType);
+            }
+
+            strongPasswordArr[positionForRandomCharacter] = randomCharacter;
+            availablePositions.remove((Integer) positionForRandomCharacter);
+        }
+        return strongPasswordArr;
+    }
+
+    private static List<Integer> createAvailablePositions(int passwordLength) {
+        List<Integer> availablePositions = new ArrayList<>();
+        for (int i = 0; i < passwordLength; i++) {
+            availablePositions.add(i);
+        }
+        return availablePositions;
+    }
 
 }
