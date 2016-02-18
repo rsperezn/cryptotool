858899b99b13c25e6ab9c15cf7da2d694f7f9de7
Santiago <rsperezn@gmail.com>
2016-02-18 21:12:33 +1100

    Cleaned CaesarsDecryption class

--- cryptoTool/src/main/java/com/rspn/cryptotool/decrypt/CaesarsDecryption.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/decrypt/CaesarsDecryption.java
@@ -5,53 +5,50 @@ import java.util.Hashtable;
 import com.rspn.cryptotool.utils.CTUtils;
 
 public class CaesarsDecryption {
-	private static char[] alphabet= CTUtils.getAlphabet();
-	private static Hashtable<Character,Integer> charToint = new Hashtable<Character,Integer>();
-	private static boolean initialized=false;
-	private static char[] decryptedText;
-	private static boolean retainCase;
-	
-	public static void initComponents(){
-		if(!initialized){
-			for(int i=0;i<alphabet.length;i++){
-				charToint.put(alphabet[i], i);
-			}
-			initialized=true;
-		}	
-	}
-	
-	public static String runDecryption(String encryptedText,int delta, boolean whitespaces){
-		decryptedText= new char[encryptedText.length()];
-		retainCase=CTUtils.retainCase;
+    private static char[] alphabet = CTUtils.getAlphabet();
+    private static Hashtable<Character, Integer> charToint = new Hashtable<>();
+    private static boolean initialized = false;
+    private static char[] decryptedText;
+    private static boolean retainCase;
 
-		if(!retainCase){
-			encryptedText=encryptedText.toUpperCase();
-		}
+    public static void initComponents() {
+        if (!initialized) {
+            for (int i = 0; i < alphabet.length; i++) {
+                charToint.put(alphabet[i], i);
+            }
+            initialized = true;
+        }
+    }
 
-		for (int i = 0; i < encryptedText.length(); i++) {
-			boolean upper = Character.isUpperCase(encryptedText.charAt(i));
-			if(charToint.containsKey(encryptedText.charAt(i))){
-				int pos= (((charToint.get(encryptedText.charAt(i))-delta) %52)+52)%52;
-				//make sure that it doesn't change 
-				if(upper && pos >25)
-					pos=pos-26;
-				else if (!upper && pos<26)
-					pos=pos+26;
-				
-				decryptedText[i]=alphabet[pos];
-			}
-			else if(encryptedText.charAt(i)=='\n'){
-				decryptedText[i]='\n';      	
-			}
-			else{
-				decryptedText[i]=encryptedText.charAt(i);
-			}
-		}
-		if(whitespaces)
-			return new String(decryptedText);
-		else
-			return new String(decryptedText).replaceAll(" ", "");
-	}
-	
-	
+    public static String runDecryption(String encryptedText, int delta, boolean whitespaces) {
+        decryptedText = new char[encryptedText.length()];
+        retainCase = CTUtils.retainCase;
+
+        if (!retainCase) {
+            encryptedText = encryptedText.toUpperCase();
+        }
+
+        for (int i = 0; i < encryptedText.length(); i++) {
+            boolean upper = Character.isUpperCase(encryptedText.charAt(i));
+            if (charToint.containsKey(encryptedText.charAt(i))) {
+                int pos = (((charToint.get(encryptedText.charAt(i)) - delta) % 52) + 52) % 52;
+                //make sure that it doesn't change
+                if (upper && pos > 25)
+                    pos = pos - 26;
+                else if (!upper && pos < 26)
+                    pos = pos + 26;
+
+                decryptedText[i] = alphabet[pos];
+            } else if (encryptedText.charAt(i) == '\n') {
+                decryptedText[i] = '\n';
+            } else {
+                decryptedText[i] = encryptedText.charAt(i);
+            }
+        }
+        if (whitespaces)
+            return new String(decryptedText);
+        else
+            return new String(decryptedText).replaceAll(" ", "");
+    }
+
 }
