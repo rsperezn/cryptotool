956c1bbc3c7aac0bdaf17565c455858f3103dce3
development <development@boost.oz.moneyswitch.net>
2016-03-21 12:50:21 +1100

    RefactoredCalculateHashesActivity to extend abstract class

--- cryptoTool/src/main/java/com/rspn/cryptotool/calculatehashes/CalculateHashesActivity.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/calculatehashes/CalculateHashesActivity.java
@@ -1,5 +1,34 @@
 package com.rspn.cryptotool.calculatehashes;
 
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.orleonsoft.android.simplefilechooser.Constants;
+import com.orleonsoft.android.simplefilechooser.ui.FileChooserActivity;
+import com.rspn.cryptotool.AbstractCryptActivity;
+import com.rspn.cryptotool.R;
+import com.rspn.cryptotool.model.NavigationItem;
+import com.rspn.cryptotool.uihelper.CryptInfoDialogFragment;
+import com.rspn.cryptotool.uihelper.OpenComparisonInputsFragment;
+import com.rspn.cryptotool.uihelper.OpenDialogFragment;
+import com.rspn.cryptotool.utils.CTUtils;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,7 +43,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -23,108 +51,48 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
-import android.app.AlertDialog;
-import android.app.FragmentManager;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Vibrator;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.orleonsoft.android.simplefilechooser.Constants;
-import com.orleonsoft.android.simplefilechooser.ui.FileChooserActivity;
-import com.rspn.cryptotool.MainActivity;
-import com.rspn.cryptotool.R;
-import com.rspn.cryptotool.uihelper.CryptInfoDialogFragment;
-import com.rspn.cryptotool.uihelper.DrawerAdapter;
-import com.rspn.cryptotool.uihelper.OpenComparisonInputsFragment;
-import com.rspn.cryptotool.uihelper.OpenDialogFragment;
-import com.rspn.cryptotool.model.NavigationItem;
-import com.rspn.cryptotool.utils.CTUtils;
+public class CalculateHashesActivity extends AbstractCryptActivity implements OnItemSelectedListener, View.OnClickListener {
 
+    private EditText input1_edit;
+    private Button calculate_bt;
+    private Button copyToClipboard_bt;
+    private String algorithm;
+    private Spinner algorithms_sp;
+    private EditText output_edit;
+    private LinearLayout calculate_layout;
+    private LinearLayout main_layout;
+    private final int FILE_CHOOSER = 1; // onActivityResult request code
+    private String hashType = "Text";
+    private CheckBox compare_chb;
+    private ProgressDialog progressDialog;
+    private long timerStartTime;
+    private long fileSize;
+    private double hashingTime;
+    private SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    private long lastModified;
+    private MenuItem hashingDetailsMenuItem;
+    private String[] fileHashingAlgorithms = new String[]{"Algorithms", "SHA-1", "SHA-256", "SHA-384", "SHA-512", "MD5"};
+    private String[] textHashingAlgorithms = new String[]{"Algorithms", "SHA-1", "SHA-256", "SHA-384", "SHA-512", "MD5", "RC4", "Blowfish", "AES"};
+    private boolean shouldShowDetailsItem = false;
+    private boolean comparingHashes = false;
 
-@SuppressWarnings("deprecation")
-public class CalculateHashesActivity extends ActionBarActivity implements OnClickListener, OnItemSelectedListener, OnItemClickListener {
-
-    EditText input1_edit;
-    Button calculate_bt;
-    Button copyToClipboard_bt;
-    String algorithm;
-    Spinner algorithms_sp;
-    EditText output_edit;
-    AdView adView;
-    CharSequence title;
-    ActionBarDrawerToggle drawerToggle;
-    Vibrator vibrator;
-    DrawerLayout navDrawerLayout;
-    ListView navList;
-    CharSequence drawerTitle;
-    List<NavigationItem> navItems;
-    LinearLayout calculate_layout;
-    LinearLayout main_layout;
-    boolean navDrawerOpen = false;
-    final int FILE_CHOOSER = 1; // onActivityResult request code
-    String hashType = "Text";
-    RadioButton type_rb;
-    CheckBox compare_chb;
-    ProgressDialog progressDialog;
-    long timerStartTime;
-    long fileSize;
-    double hashingTime;
-    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-    long lastModified;
-    MenuItem hashingDetailsMenuItem;
-    String[] fileHashingAlgorithms = new String[]{"Algorithms", "SHA-1", "SHA-256", "SHA-384", "SHA-512", "MD5"};
-    String[] textHashingAlgorithms = new String[]{"Algorithms", "SHA-1", "SHA-256", "SHA-384", "SHA-512", "MD5", "RC4", "Blowfish", "AES"};
-    boolean shouldShowDetailsItem = false;
-    boolean comparingHashes = false;
+    public CalculateHashesActivity() {
+        super(R.layout.activity_calculatehashes,
+                R.id.adView_InCalculateHashesActivity,
+                R.id.drawer_layoutInCalculateHashesActivity,
+                R.id.navigation_listInCalculateHashesActivity,
+                R.drawable.xor_binary);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculatehashes);
-        if (this.getIntent().hasExtra("HashType")) {
-            hashType = this.getIntent().getStringExtra("HashType");
-            setTitle("Calculate Hashes of " + hashType);
-        }
-
+    protected void findAndSetViews() {
         input1_edit = (EditText) findViewById(R.id.edit_inputHash);
         calculate_bt = (Button) findViewById(R.id.calculate_hash);
-        calculate_bt.setOnClickListener(this);
         algorithms_sp = (Spinner) findViewById(R.id.spinner_hashAlgorithms);
         ArrayAdapter<String> algorithmsAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item, textHashingAlgorithms);
         algorithms_sp.setAdapter(algorithmsAdapter);
-
-        algorithms_sp.setOnItemSelectedListener(this);
         setSpinnerAlgorithms();
         calculate_layout = (LinearLayout) findViewById(R.id.bottomCalculateHashes_layout);
         main_layout = (LinearLayout) findViewById(R.id.mainCalculateHashes_layout);
@@ -134,59 +102,45 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
         copyToClipboard_bt.setVisibility(View.INVISIBLE);
         compare_chb = (CheckBox) findViewById(R.id.checkBox_compareHashes);
         compare_chb.setOnClickListener(this);
-
-        navDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layoutInCalculateHashesActivity);
-        navDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
-        View header = getLayoutInflater().inflate(R.layout.header_navigation, null);
-        ImageView iv = (ImageView) header.findViewById(R.id.header);
-        iv.setImageResource(R.drawable.xor_binary);
-        navList = (ListView) findViewById(R.id.navigation_listInCalculateHashesActivity);
-        navList.addHeaderView(header);
-        navList.setOnItemClickListener(this);
-        navItems = new ArrayList<>();
-        navItems.add(new NavigationItem("Home", R.drawable.ic_home));
-        navItems.add(new NavigationItem("Share", R.drawable.ic_share));
-        navItems.add(new NavigationItem("Info", R.drawable.ic_info));
-        DrawerAdapter adapter = new DrawerAdapter(this, navItems);
-        navList.setAdapter(adapter);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        title = drawerTitle = getTitle();
-        drawerToggle = new ActionBarDrawerToggle(this, navDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
-            public void onDrawerClosed(View view) {
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-                navDrawerOpen = false;
-            }
-
-            public void onDrawerOpened(View drawerView) {
-                hideKeyboard();
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-                navDrawerOpen = true;
-            }
-        };
-
-        navDrawerLayout.setDrawerListener(drawerToggle);
         setEditTextsHints();
-        //Ads
-        adView = (AdView) this.findViewById(R.id.adView_InCalculateHashesActivity);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
+    }
 
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    @Override
+    protected void drawerItemClick(int position) {
+        NavigationItem navigationItem = ((NavigationItem) navigationList.getItemAtPosition(position));
+        if (navigationItem == null) {
+            return;
+        }
+        String selectedOption = navigationItem.getItemName();
+        if (selectedOption.equals("Info")) {
+            FragmentManager manager = getFragmentManager();
+            CryptInfoDialogFragment dialog = CryptInfoDialogFragment.newInstance("calculateHashes");
+            dialog.show(manager, "dialog");
+        }
+    }
 
-        //		String all="";
-        //		for (Provider provider : Security.getProviders()) {
-        //            System.out.println("Provider: " + provider.getName());
-        //            all+= "Provider: " + provider.getName();
-        //            for (Provider.Service service : provider.getServices()) {
-        //                System.out.println("  Algorithm: " + service.getAlgorithm());
-        //                all +=  "  Algorithm: " + service.getAlgorithm();
-        //            }
-        //		}
-        //
-        //		input_edit.setText(all);
+    @Override
+    protected String getSharableContent() {
+        return output_edit.getText().toString();
+    }
+
+    @Override
+    protected boolean satisfiedMainButtonPreconditions() {
+        return false;
+    }
+
+    @Override
+    protected void setOnClickListener() {
+        calculate_bt.setOnClickListener(this);
+        algorithms_sp.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    protected void setDataFromOriginatingActivity() {
+        if (this.getIntent().hasExtra("HashType")) {
+            hashType = this.getIntent().getStringExtra("HashType");
+            setTitle("Calculate Hashes of " + hashType);
+        }
     }
 
     @Override
@@ -205,16 +159,9 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
                 }
 
             case R.id.copyToClipboard_hash:
-                String hashedData = output_edit.getText().toString();
-                if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
-                    android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    clipboard.setText(hashedData);
-                } else {
-                    android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    android.content.ClipData clip = android.content.ClipData.newPlainText("Copy of Hash data", hashedData);
-                    clipboard.setPrimaryClip(clip);
-                }
-                startVibrate(15);
+                String hashedData = getSharableContent();
+                copyToClipboard(hashedData);
+                vibrate(15);
                 break;
 
             case R.id.checkBox_compareHashes:
@@ -275,14 +222,14 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
             data.add(output_edit.getText().toString());
         }
 
-        HashData hasherDTO = new HashData(algorithm, hashType, data);
+        HashData hashData = new HashData(algorithm, hashType, data);
 
         progressDialog = new ProgressDialog(this);
         timerStartTime = System.currentTimeMillis();
         progressDialog.setMessage("Calculating hash of " + hashType + " ...");
         progressDialog.show();
-        new Hasher().execute(hasherDTO);
-        startVibrate();
+        new Hasher().execute(hashData);
+        vibrate();
     }
 
     public void displayHashComparisonResult(boolean matching) {
@@ -291,8 +238,7 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position,
-                               long id) {
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (position == 0) {
             calculate_bt.setEnabled(false);
         } else {
@@ -307,81 +253,11 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
     }
 
     @Override
-    public void setTitle(CharSequence title) {
-        this.title = title;
-        getSupportActionBar().setTitle(this.title);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
-
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    public void startVibrate() {
-        if (CTUtils.vibrate) {
-            vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            vibrator.vibrate(50);
-        }
-    }
-
-    public void startVibrate(int length) {
-        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        vibrator.vibrate(length);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position,
-                            long id) {
-        NavigationItem navigationItem = ((NavigationItem) navList.getItemAtPosition(position));
-        if (navigationItem == null) {
-            return;
-        }
-        String selectedOption = navigationItem.getItemName();
-
-        switch (selectedOption) {
-            case "Share":
-                Intent shareIntent = new Intent();
-                shareIntent.setAction(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, output_edit.getText().toString());
-                startActivity(shareIntent);
-                break;
-            case "Home":
-                Intent intent = new Intent(this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                break;
-            case "Info":
-                FragmentManager manager = getFragmentManager();
-                CryptInfoDialogFragment dialog = CryptInfoDialogFragment.newInstance("calculateHashes");
-                dialog.show(manager, "dialog");
-                break;
-        }
-    }
-
-    public void hideKeyboard() {
-        InputMethodManager imm = (InputMethodManager) getSystemService(
-                Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(input1_edit.getWindowToken(), 0);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.calculate_hash, menu);
         hashingDetailsMenuItem = menu.getItem(1);
         hashingDetailsMenuItem.setVisible(false);
         return true;
-
     }
 
     @Override
@@ -481,7 +357,7 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        if (!navDrawerOpen && shouldShowDetailsItem && hashType.equals("File")) {
+        if (!isNavigationDrawerOpen() && shouldShowDetailsItem && hashType.equals("File")) {
             hashingDetailsMenuItem.setVisible(true);
         }
         return super.onPrepareOptionsMenu(menu);
@@ -496,26 +372,18 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
         return true;
     }
 
-    //Async task
-    class Hasher extends AsyncTask<HashData, Integer, Void> {
-        EditText output_edit;
-        String hashType;
-        ArrayList<String> dataToHash;
-        String algorithm;
-        ArrayList<String> hashResults = new ArrayList<>();
-        boolean areMatchingHashes = false;
-
-        @Override
-        protected void onPreExecute() {
-            output_edit = (EditText) findViewById(R.id.edit_hashedText);
-
-        }
+    private class Hasher extends AsyncTask<HashData, Integer, Void> {
+        private String hashType;
+        private ArrayList<String> dataToHash;
+        private String algorithm;
+        private ArrayList<String> hashResults = new ArrayList<>();
+        private boolean areMatchingHashes = false;
 
         @Override
         protected Void doInBackground(HashData... params) {
-            hashType =  params[0].getHashType();
+            hashType = params[0].getHashType();
             dataToHash = params[0].getData();
-            algorithm =  params[0].getAlgorithm();
+            algorithm = params[0].getAlgorithm();
             for (String data : dataToHash) {
                 if (hashType.equals("Text")) {
                     calculateHashOfText(data);
@@ -581,7 +449,7 @@ public class CalculateHashesActivity extends ActionBarActivity implements OnClic
             byteArray = text.getBytes();
 
             try {
-                hideKeyboard();
+                hideKeyboard(input1_edit);
                 if (algorithm.equals("Blowfish")) {
                     KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
                     keyGenerator.init(128);
