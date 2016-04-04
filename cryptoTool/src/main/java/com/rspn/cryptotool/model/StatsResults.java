c8a61de49e65ce910e7ea23f160a0324bcc4a9e3
Santiago <rsperezn@gmail.com>
2016-04-04 21:47:38 +1000

    Cleaned StatsResults.

--- cryptoTool/src/main/java/com/rspn/cryptotool/model/StatsResults.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/model/StatsResults.java
@@ -5,58 +5,6 @@ import android.os.Parcelable;
 
 public class StatsResults implements Parcelable{
 	
-	String resultType;
-	int[] charCount = new int[25];
-	double [] ICforKeyLength = new 	double[25];
-	
-	public void setResultType(String type){
-		resultType=type;
-	}
-	
-	public String getResultType(){
-		return resultType;		
-	}
-	
-	public void setCharCount(int[] charCount){
-		this.charCount=charCount;
-	}
-	
-	public int[] getCharCount(){
-		return charCount;
-	}
-	
-	public void setICforKeyLength(double [] ic){
-		this.ICforKeyLength=ic;
-	}
-	
-	public double[] getICforKeyLength(){
-		return ICforKeyLength;		
-	}
-	
-	public StatsResults(){
-		
-	}
-	
-	public StatsResults(Parcel in){
-		resultType=in.readString();
-		charCount=in.createIntArray();
-		ICforKeyLength=in.createDoubleArray();
-		
-	}
-
-	@Override
-	public int describeContents() {
-		return 0;
-	}
-
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeString(resultType);
-		dest.writeIntArray(charCount);
-		dest.writeDoubleArray(ICforKeyLength);
-		
-	}
-	
 	public static final Parcelable.Creator<StatsResults> CREATOR =
 			new Parcelable.Creator<StatsResults>() {
 
@@ -69,7 +17,58 @@ public class StatsResults implements Parcelable{
 				public StatsResults[] newArray(int size) {
 					return new StatsResults[size];
 				}
-				
+
 			};
+	private String resultType;
+	private int[] charCount = new int[25];
+	private double [] ICforKeyLength = new 	double[25];
+	
+	public StatsResults(){
+
+	}
+	
+	public StatsResults(Parcel in){
+		resultType=in.readString();
+		charCount=in.createIntArray();
+		ICforKeyLength=in.createDoubleArray();
+
+	}
+	
+	public String getResultType(){
+		return resultType;
+	}
+	
+	public void setResultType(String type){
+		resultType=type;
+	}
+	
+	public int[] getCharCount(){
+		return charCount;
+	}
+	
+	public void setCharCount(int[] charCount){
+		this.charCount=charCount;
+	}
+	
+	public double[] getICforKeyLength(){
+		return ICforKeyLength;
+	}
+
+	public void setICforKeyLength(double [] ic){
+		this.ICforKeyLength=ic;
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+	
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(resultType);
+		dest.writeIntArray(charCount);
+		dest.writeDoubleArray(ICforKeyLength);
+
+	}
 
 }
