b0dce2a0ef1b7a3ea35597dc0f68cf3a4608e4c1
Santiago <rsperezn@gmail.com>
2016-04-04 21:43:21 +1000

    Cleaned NavigationItem.

--- cryptoTool/src/main/java/com/rspn/cryptotool/model/NavigationItem.java
+++ cryptoTool/src/main/java/com/rspn/cryptotool/model/NavigationItem.java
@@ -2,10 +2,9 @@ package com.rspn.cryptotool.model;
 
 public class NavigationItem {
 
-	String ItemName;
-	int imgResID;
+	private String ItemName;
+	private int imgResID;
 
-	
 	public NavigationItem(String itemName, int imgResID) {
 		super();
 		ItemName = itemName;
